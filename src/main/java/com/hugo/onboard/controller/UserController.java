4ca9d500543bb26a474138b227eb1dd1fb318c0c
HugoSujith <sujith.dhaipule@hugohub.com>
2025-01-15 17:13:22 +0530

    User functionality added

--- src/main/java/com/hugo/onboard/controller/UserController.java
+++ src/main/java/com/hugo/onboard/controller/UserController.java
@@ -2,8 +2,11 @@ package com.hugo.onboard.controller;
 
 import com.hugo.onboard.model.user.User;
 import com.hugo.onboard.service.UserService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,9 +20,18 @@ public class UserController {
         this.userService = userService;
     }
 
-    @PostMapping("register")
-    public String registerUser(@RequestBody User user) {
-        userService.addUserToDatabase(user);
-        return "User added to database";
+    @PostMapping("registerUser")
+    public ResponseEntity<String> registerUser(@RequestBody User user) {
+        return userService.addUserToDatabase(user);
+    }
+
+    @PostMapping("loginUser")
+    public ResponseEntity<String> loginUser(@RequestBody User user) {
+        return userService.authenticateUser(user);
+    }
+
+    @PutMapping("updateUser")
+    public ResponseEntity<String> updateUserInfo(@RequestBody User user) {
+        return userService.updateUserInfo(user);
     }
 }
--- src/main/java/com/hugo/onboard/service/UserService.java
+++ src/main/java/com/hugo/onboard/service/UserService.java
@@ -1,7 +1,11 @@
 package com.hugo.onboard.service;
 
+import java.util.Optional;
+
 import com.hugo.onboard.model.user.User;
 import com.hugo.onboard.repository.UserRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,7 +16,44 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
-    public void addUserToDatabase(User user) {
-        userRepository.save(user);
+    public Boolean findUserIfPresent(User user) {
+        Optional<User> searchDBForUser = userRepository.findById(user.getUsername());
+        return searchDBForUser.isPresent();
+    }
+
+    public ResponseEntity<String> addUserToDatabase(User user) {
+        try {
+            if (Boolean.FALSE.equals(findUserIfPresent(user))) {
+                userRepository.save(user);
+                return new ResponseEntity<>("User registered", HttpStatus.OK);
+            }
+            return new ResponseEntity<>("User already registered", HttpStatus.CONFLICT);
+        } catch (Exception e) {
+            return new ResponseEntity<>("User registration failed", HttpStatus.EXPECTATION_FAILED);
+        }
+    }
+
+    public ResponseEntity<String> authenticateUser(User user) {
+        Optional<User> foundUser = userRepository.findById(user.getUsername());
+        if (foundUser.isEmpty()) {
+            return new ResponseEntity<>("No such user found", HttpStatus.NOT_FOUND);
+        }
+        User existingUser = foundUser.get();
+        if (user.getPassword().equals(existingUser.getPassword())) {
+            return new ResponseEntity<>("User is authorized to use the platform", HttpStatus.OK);
+        }
+        return new ResponseEntity<>("Invalid password", HttpStatus.UNAUTHORIZED);
+    }
+
+    public ResponseEntity<String> updateUserInfo(User user) {
+        Optional<User> foundUser = userRepository.findById(user.getUsername());
+        if (foundUser.isPresent()) {
+            if (Boolean.FALSE.equals(foundUser.get().getBalance().equals(user.getBalance()))) {
+                return new ResponseEntity<>("You are trying to manipulate your balance", HttpStatus.BAD_REQUEST);
+            }
+            userRepository.save(user);
+            return new ResponseEntity<>("Your details updated", HttpStatus.OK);
+        }
+        return new ResponseEntity<>("You are not registered. Please register yourself first!", HttpStatus.NOT_FOUND);
     }
 }
